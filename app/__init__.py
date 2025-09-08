def4b95b7a14974e57c57f109c976ed903c18bb8
Chinmay R M <csechinmayrm@gmail.com>
2025-09-08 19:14:41 +0530

    Update database URI for Render deployment

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,10 @@ CUSTOM_SECRET = 'change-this-to-a-random-string-1234567890'
 app = Flask(__name__)
 app.secret_key = CUSTOM_SECRET
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/attendance_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+	'DATABASE_URL',
+	'postgresql://studentattendence_u1rx_user:q7ayUsTp2Ul2iCqHeCy1VJetoQ1lqEJQ@dpg-d2srajumcj7s73ah3dl0-a.oregon-postgres.render.com/studentattendence_u1rx'
+)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
